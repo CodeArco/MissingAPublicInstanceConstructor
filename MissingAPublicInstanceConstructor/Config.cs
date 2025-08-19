0bd72e6d535d718d935c25d2e56f6369ea0f9bc8
CodeArco <codearconet@gmail.com>
2025-08-19 07:06:07 +0200

    Initial commit

---
+++ MissingAPublicInstanceConstructor/Config.cs
@@ -0,0 +1,23 @@
+﻿namespace MissingAPublicInstanceConstructor
+{
+    public class Config
+    {
+        public Config() { }
+        public string PropString { get; set; }
+        public int PropInt { get; set; }
+        public Class1 PropClass1 { get; set; }
+        public Class2 PropClass2 { get; set; }
+    }
+    public class Class1
+    {
+        public Class1() { }
+        public string PropString { get; set; }
+        public int PropInt { get; set; }
+    }
+    public class Class2
+    {
+        public Class2() { }
+        public string PropString { get; set; }
+        public int PropInt { get; set; }
+    }
+}
---
+++ MissingAPublicInstanceConstructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using MissingAPublicInstanceConstructor;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var build = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+        // Alternative 1
+        var config1 = build.Get<Config>();
+
+        // Alternative 2
+        var config2 = new Config();
+        build.Bind(config2);
+
+        // Output
+        Console.WriteLine($"config1?.PropString: '{config1?.PropString}'");
+        Console.WriteLine($"config1?.PropClass1.PropString: '{config1?.PropClass1.PropString}'");
+
+        Console.WriteLine($"config2.PropString: '{config2.PropString}'");
+        Console.WriteLine($"config2.PropClass1.PropString: '{config2.PropClass1.PropString}'");
+    }
+}
